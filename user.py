8f0269ec164965cea84c587400badc5d1aeeea04
Suzumiya <jrgwyz@163.com>
2017-01-05 17:32:20 +0800

    Update user.py

--- user.py
+++ user.py
@@ -9,8 +9,7 @@ from flask import session
 from models import User
 
 
-# 创建一个 蓝图对象 并且路由定义在蓝图对象中
-# 然后在 flask 主代码中「注册蓝图」来使用
+# 创建一个 蓝图对象 并且路由定义在蓝图对象中 然后在 flask 主代码中「注册蓝图」来使用
 # 第一个参数是蓝图的名字，第二个参数是套路
 main = Blueprint('user', __name__)
 
